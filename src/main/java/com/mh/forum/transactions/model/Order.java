5beda7f4953f427e287f9612fa27ecb2e2b89486
m6hichri <m6hichri@enib.fr>
2020-07-15 01:16:46 +0200

    fix issues<

--- src/main/java/com/mh/forum/transactions/model/Order.java
+++ src/main/java/com/mh/forum/transactions/model/Order.java
@@ -13,8 +13,8 @@ public class Order {
 
 	private double price;
 	private String currency;
-	private String method;
-	private String intent;
+	//private String method;
+	//private String intent;
 	//private String description;
 
 }
